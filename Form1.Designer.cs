85b69eb4b3657c75cdad7c5c271d56afb6ed5d5c
Yanlong Li <44524468+DragonPara@users.noreply.github.com>
2023-01-27 14:46:13 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace light
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.升高亮度ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.降低亮度ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.闪烁ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.退出ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.升高亮度ToolStripMenuItem,
+            this.降低亮度ToolStripMenuItem,
+            this.闪烁ToolStripMenuItem,
+            this.退出ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(149, 92);
+            // 
+            // 升高亮度ToolStripMenuItem
+            // 
+            this.升高亮度ToolStripMenuItem.Name = "升高亮度ToolStripMenuItem";
+            this.升高亮度ToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
+            this.升高亮度ToolStripMenuItem.Text = "升高亮度";
+            this.升高亮度ToolStripMenuItem.Click += new System.EventHandler(this.显示ToolStripMenuItem_Click);
+            // 
+            // 降低亮度ToolStripMenuItem
+            // 
+            this.降低亮度ToolStripMenuItem.Name = "降低亮度ToolStripMenuItem";
+            this.降低亮度ToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
+            this.降低亮度ToolStripMenuItem.Text = "降低亮度";
+            this.降低亮度ToolStripMenuItem.Click += new System.EventHandler(this.隐藏ToolStripMenuItem_Click);
+            // 
+            // 闪烁ToolStripMenuItem
+            // 
+            this.闪烁ToolStripMenuItem.Name = "闪烁ToolStripMenuItem";
+            this.闪烁ToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
+            this.闪烁ToolStripMenuItem.Text = "闪烁托盘图标";
+            this.闪烁ToolStripMenuItem.Click += new System.EventHandler(this.闪烁ToolStripMenuItem_Click);
+            // 
+            // 退出ToolStripMenuItem
+            // 
+            this.退出ToolStripMenuItem.Name = "退出ToolStripMenuItem";
+            this.退出ToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
+            this.退出ToolStripMenuItem.Text = "退出";
+            this.退出ToolStripMenuItem.Click += new System.EventHandler(this.退出ToolStripMenuItem_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "light";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ToolStripMenuItem 升高亮度ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 降低亮度ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 闪烁ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 退出ToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace light
+{
+    public struct RAMP
+    {
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 256)]
+        public UInt16[] Red;
+
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 256)]
+        public UInt16[] Green;
+
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 256)]
+        public UInt16[] Blue;
+    }
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetDC(IntPtr hWnd);
+        [DllImport("gdi32.dll")]
+        public static extern bool GetDeviceGammaRamp(IntPtr hDC, ref RAMP lpRamp);
+        [DllImport("gdi32.dll")]
+        public static extern bool SetDeviceGammaRamp(IntPtr hDC, ref RAMP lpRamp);
+
+
+        private Icon light = Properties.Resources.sun_2;
+        private Icon black = Properties.Resources.sun;
+        private bool _status = true;
+        private bool _shine = false; //shine 闪烁
+        private short BRIGHTNESS_MAX = 100;
+        private short BRIGHTNESS_MIN = 10;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void 显示ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            short max, min, cur;
+            max = min = cur = 0;
+            GetBrightness(ref min, ref cur, ref max);
+            cur += 10;
+            if (cur > BRIGHTNESS_MAX)
+            {
+                notifyIcon1.ShowBalloonTip(5000, "不能再亮了哥","最大亮度100", ToolTipIcon.Info);
+                return;
+            }
+            SetBrightness(cur); 
+            notifyIcon1.ShowBalloonTip(5000, "当前亮度信息", "当前亮度" + cur + "|最小亮度" + min + "|最大亮度" + max, ToolTipIcon.Info);
+        }
+
+        private void 隐藏ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Visible = false;
+            short max, min, cur;
+            max = min = cur = 0;
+            GetBrightness(ref min, ref cur, ref max);
+            cur -= 10;
+            if (cur < BRIGHTNESS_MIN)
+            {
+                notifyIcon1.ShowBalloonTip(5000, "不能再暗了哥", "最小亮度10", ToolTipIcon.Info);
+                return;
+            }
+            SetBrightness(cur);
+            notifyIcon1.ShowBalloonTip(5000, "当前亮度信息", "当前亮度" + cur + "|最小亮度" + min + "|最大亮度" + max, ToolTipIcon.Info);
+        }
+
+        private void 退出ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (_status)
+            {
+                notifyIcon1.Icon = light;
+            }
+            else
+            {
+                notifyIcon1.Icon = black; 
+            }
+            _status= !_status;
+        }
+
+        private void 闪烁ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (!_shine)
+            {
+                _shine = true;
+                timer1.Enabled = true;
+                timer1.Start(); 
+            }
+            else
+            {
+                notifyIcon1.Icon = light;
+                _shine = false;
+                timer1.Stop();
+                notifyIcon1.ShowBalloonTip(5000, "停止闪烁", "已停止闪烁图标", ToolTipIcon.Info);
+            }
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+
+            this.Visible = false;
+        }
+        private double CalColorGammaVal(ushort[] line)
+        {
+            var max = line.Max();
+            var min = line[0];
+            var index = Array.FindIndex(line, n => n == max);
+            var gamma = Math.Round((((double)(max - min) / index) / 255), 2);
+            return gamma;
+        }
+        private double CalAllGammaVal(RAMP ramp)
+        {
+            return Math.Round(((CalColorGammaVal(ramp.Blue) + CalColorGammaVal(ramp.Red) +
+                                CalColorGammaVal(ramp.Green)) / 3), 2);
+        }
+        public bool GetBrightness(ref short minBrightness, ref short currentBrightness,
+            ref short maxBrightness)
+        {
+            var handle = GetDC(IntPtr.Zero);
+            //0-50 亮度变化太小，所以从50开始
+            minBrightness = BRIGHTNESS_MIN;
+            maxBrightness = BRIGHTNESS_MAX;
+            var ramp = default(RAMP);
+            var deviceGammaRamp = GetDeviceGammaRamp(handle, ref ramp);
+            currentBrightness = (short)((deviceGammaRamp ? CalAllGammaVal(ramp) : 0.5) * 100);
+            return deviceGammaRamp;
+        }
+        public bool SetBrightness(short brightness)
+        {
+            var handle = GetDC(IntPtr.Zero);
+            double value = (double)brightness / 100;
+            RAMP ramp = default(RAMP);
+            ramp.Red = new ushort[256];
+            ramp.Green = new ushort[256];
+            ramp.Blue = new ushort[256];
+
+            for (int i = 1; i < 256; i++)
+            {
+                var tmp = (ushort)(i * 255 * value);
+                ramp.Red[i] = ramp.Green[i] = ramp.Blue[i] = Math.Max(ushort.MinValue, Math.Min(ushort.MaxValue, tmp));
+            }
+
+            var deviceGammaRamp = SetDeviceGammaRamp(handle, ref ramp);
+            return deviceGammaRamp;
+        }
+    }
+}
